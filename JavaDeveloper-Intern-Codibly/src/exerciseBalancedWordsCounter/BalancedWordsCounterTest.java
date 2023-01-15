b0c0130516f9408bc9b5e383f5ccf5c3e4a140d4
Szymon Gurdek <szymon.piotr.gurdek@gmail.com>
2023-01-15 15:20:51 +0100

    Create package named exerciseBalancedWordsCounter with class BalancedWordsCounter and class BalancedWordsCounterTest(driver class) with some comments
    git status

---
+++ JavaDeveloper-Intern-Codibly/src/exerciseBalancedWordsCounter/BalancedWordsCounterTest.java
@@ -0,0 +1,13 @@
+package exerciseBalancedWordsCounter;
+
+// Note: I didn't know if You want me to test these cases in JUnit or not,
+// so the 1st exercise (BubbleSort) is tested with JUnit to prove my skills
+// and this exercise (BalancedWordsCounter) is tested via normal-driver class
+
+public class BalancedWordsCounterTest {
+
+	public static void main(String[] args) {
+
+	}
+
+}
