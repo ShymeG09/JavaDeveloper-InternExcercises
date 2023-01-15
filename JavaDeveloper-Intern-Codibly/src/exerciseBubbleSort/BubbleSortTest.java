e8e4e23a970101765dec471d8cda539c9d1f8833
Szymon Gurdek <szymon.piotr.gurdek@gmail.com>
2023-01-15 14:56:46 +0100

    Create a new class BubbleSortTest in JUnit for test cases

---
+++ JavaDeveloper-Intern-Codibly/src/exerciseBubbleSort/BubbleSortTest.java
@@ -0,0 +1,19 @@
+package exerciseBubbleSort;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class BubbleSortTest {
+	// 1. input = [1, 4, 5, 6, 8, 3, 8] result = [1, 3, 4, 5, 6, 8, 8]
+	// 2. input = [-9.3, 0.2, 4, 0.1, 5, 9] result = [-9.3, 0.1, 0.2, 4, 5, 9] 
+	// 3. input = [] result = [] 
+	// 4. input = [null, 5.0001] result = [5.0001] 
+	// 5. input = null result = RuntimeException
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
